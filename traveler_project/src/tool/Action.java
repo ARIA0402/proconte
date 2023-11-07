f3c3565ddd73c8fe9069f0b8d485e8a7733b3af5
自分の名前 <h.urata2002@gmail.com>
2023-11-07 12:20:20 +0900

    first

--- traveler_project/src/tool/Action.java
+++
@@ -1,9 +0,0 @@
-package tool;
-
-import javax.servlet.http.*;
-
-public abstract class Action {
-	public abstract String execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception;
-}
